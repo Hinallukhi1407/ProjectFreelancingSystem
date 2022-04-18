1cebfad42d88f3e99655a06424e5a69adcb02360
aakashmauryabbd <101312659+aakashmauryabbd@users.noreply.github.com>
2022-04-18 12:13:01 +0530

    Delete MicroServices/Admin/src/main/java/com/example/demo directory

--- MicroServices/Admin/src/main/java/com/example/demo/Client/MemberShipPlanRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Client;
-
-import com.example.demo.Models.Membership;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberShipPlanRepository extends JpaRepository<Membership,Integer> {
-}

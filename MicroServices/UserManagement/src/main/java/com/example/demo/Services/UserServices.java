8fe382bd97b7dbf2cd6c6fb852ae8b24cd69c70a
bbdnet10192 <aakash@bbd.co.za>
2022-04-18 15:14:24 +0530

    userprofile update fixed.

--- MicroServices/UserManagement/src/main/java/com/example/demo/Services/UserServices.java
+++ MicroServices/UserManagement/src/main/java/com/example/demo/Services/UserServices.java
@@ -2,6 +2,7 @@ package com.example.demo.Services;
 
 import com.example.demo.Client.UserRepository;
 import com.example.demo.Models.Userprofile;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -36,18 +37,29 @@ public class UserServices {
     }
     public Userprofile Update(Userprofile user,Integer id)
     {
-        user.setId(id);
-        Optional<Userprofile> emp1= repository.findById(id);
-        if(emp1.isPresent())
-        {
-            Userprofile e1=emp1.get();
-            e1.setCompanyName(user.getCompanyName());
-            e1.setProfileImage(user.getProfileImage());
-            e1.setCity(user.getCity());
-            e1.setMobileNo(user.getMobileNo());
-            return repository.save(e1);
+//        user.setId(id);
+//        Optional<Userprofile> emp1= repository.findById(id);
+//        if(emp1.isPresent())
+//        {
+//            Userprofile e1=emp1.get();
+//            e1.setCompanyName(user.getCompanyName());
+//            e1.setProfileImage(user.getProfileImage());
+//            e1.setCity(user.getCity());
+//            e1.setMobileNo(user.getMobileNo());
+//            return repository.save(e1);
+//        }
+//
+//        return  null;
+        if(repository.existsById(id)){
+            Userprofile currentProfile = repository.findUserById(id);
+            user.setId(id);
+            if(user.getCity() == null) user.setCity(currentProfile.getCity());
+            if (user.getLogin() == null) user.setLogin(currentProfile.getLogin());
+            return repository.save(user);
+        } else {
+            return null;
         }
 
-        return  null;
+
     }
 }
